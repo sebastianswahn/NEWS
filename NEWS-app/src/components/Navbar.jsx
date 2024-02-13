3cd6c2fc930f6610518d54e55bc689c8c19d8008
Sebastian Swahn <sebbe.swahn@hotmail.se>
2024-02-13 12:54:23 +0100

    fixing navbar functionality

--- NEWS-app/src/components/Navbar.jsx
+++ NEWS-app/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export function Header() {
+export function Navbar() {
   return (
-    <div className="bg-blue-400 flex width-screen h-16">
-      <ul className="flex">
-        <li>Calculate</li> //Link to CalcForm (need to setup router first)
-        <li>See graph</li> //Link to GraphResults (need to setup router first)
+    <div className="bg-blue-400 flex width-screen h-12">
+      <ul className="flex w-full justify-center pt-4">
+        <li className="mr-12 cursor-pointer hover:underline">
+          <NavLink to="/">New evaluation</NavLink>
+        </li>
+        <li className="ml-12 cursor-pointer hover:underline">
+          <NavLink to="/graph">See graph</NavLink>
+        </li>
       </ul>
     </div>
   );
 }
 
-export default Header;
+export default Navbar;
